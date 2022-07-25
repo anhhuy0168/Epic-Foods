386221303f32bb5ea4d83d910f71e04c4bab8e84
anhhuy0168 <anhhuy123zxc123@gmail.com>
2022-07-25 12:04:39 +0700

    Create loadUser to authenticate User

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,7 +1,8 @@
-import {createContext,useReducer } from 'react'
+import {createContext,useReducer, useEffect } from 'react'
 import { authReducer } from '../reducers/authReducer'
 import {apiUrl,LOCAL_STORAGE_TOKEN_NAME} from'./constants'
 import axios from 'axios'
+import setAuthToken from '../utils/setAuthToken'
 export const AuthContext = createContext()
 const AuthContextProvider = ({children})=>{
     const [authState , dispatch] = useReducer(authReducer,{
@@ -9,6 +10,31 @@ const AuthContextProvider = ({children})=>{
         isAuthenticated:false,
         user:null
     })
+    // check user
+    const loadUser = async () => {
+		if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
+			setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME])
+		}
+
+		try {
+			const response = await axios.get(`${apiUrl}/auth/`)
+			if (response.data.success) {
+				dispatch({
+					type: 'SET_AUTH',
+					payload: { isAuthenticated: true, user: response.data.user }
+				})
+			}
+		} catch (error) {
+			localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME)
+			setAuthToken(null)
+			dispatch({
+				type: 'SET_AUTH',
+				payload: { isAuthenticated: false, user: null }
+			})
+		
+		}
+	}
+	useEffect(() => loadUser(), [])
     //login
 
     const loginUser = async userForm =>{
@@ -18,6 +44,8 @@ const AuthContextProvider = ({children})=>{
             if(response.data.success)
             localStorage.setItem(LOCAL_STORAGE_TOKEN_NAME ,response.data.accessToken)
             console.log(response.data.accessToken,"day la token")
+            await loadUser()
+
             return response.data
         } catch (error) {
             //send req in back-end
