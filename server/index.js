e45c7feaa2f6965ff133aa64bd8619fff8df4fd6
anhhuy0168 <anhhuy123zxc123@gmail.com>
2022-08-24 12:33:06 +0700

    Design and change something

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/foods", foodsRouter);
 app.use("/auth/cart", cartRouter);
 app.use("/api/admin", adminRouter);
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 // // app.use('/api/auth', authRouter)
