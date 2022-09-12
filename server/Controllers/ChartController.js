9535d3b34537ec6d97a04b3429f1f208493ba36b
anhhuy0168 <anhhuy123zxc123@gmail.com>
2022-09-12 11:29:14 +0700

    Done view chart in day and month

--- server/Controllers/ChartController.js
+++ server/Controllers/ChartController.js
@@ -65,5 +65,90 @@ class ChartController {
       },
     });
   }
+  async getChartOfMonth(req, res) {
+    const totalOFMonth = await Order.aggregate([
+      {
+        $group: {
+          _id: { $month: "$createdAt" },
+          TotalMoney: {
+            $sum: "$price",
+          },
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          label: {
+            $switch: {
+              branches: [
+                {
+                  case: { $eq: ["$_id", 1] },
+                  then: "Jan",
+                },
+                {
+                  case: { $eq: ["$_id", 2] },
+                  then: "Feb",
+                },
+                {
+                  case: { $eq: ["$_id", 3] },
+                  then: "Mar",
+                },
+                {
+                  case: { $eq: ["$_id", 4] },
+                  then: "Apr",
+                },
+                {
+                  case: { $eq: ["$_id", 5] },
+                  then: "Maу",
+                },
+                {
+                  case: { $eq: ["$_id", 6] },
+                  then: "Jun",
+                },
+                {
+                  case: { $eq: ["$_id", 7] },
+                  then: "Jul",
+                },
+                {
+                  case: { $eq: ["$_id", 8] },
+                  then: "Aug",
+                },
+                {
+                  case: { $eq: ["$_id", 9] },
+                  then: "Sep",
+                },
+                {
+                  case: { $eq: ["$_id", 10] },
+                  then: "Oct",
+                },
+                {
+                  case: { $eq: ["$_id", 11] },
+                  then: "Noᴠ",
+                },
+                {
+                  case: { $eq: ["$_id", 12] },
+                  then: "Dec",
+                },
+              ],
+              default: "Unknown",
+            },
+          },
+          TotalMoney: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      status: "success",
+      data: {
+        totalOFMonth,
+      },
+    });
+  }
 }
 module.exports = new ChartController();
--- server/routes/statistic.js
+++ server/routes/statistic.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ChartController = require("../Controllers/ChartController");
 router.get("/get_chart/dateOfWeek", ChartController.getChart);
+router.get("/get_chart/ofMonth", ChartController.getChartOfMonth);
+
 module.exports = router;
